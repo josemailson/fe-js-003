3f3f1f40aae59a80328b8b3758d7e2a161f15fd7
josemailson <jmailsons@gmail.com>
2023-02-10 21:50:25 -0300

    testing

--- exemplos/teste.js
+++ exemplos/teste.js
@@ -1,39 +1,23 @@
-// const Opcao = require('./Opcao');
 import { Opcao } from "./Opcao.js";
 
 let listaAtividades = baixarLista();
 console.log(listaAtividades);
 
-// Criar novos elementos
+
 const formulario = document.createElement('form');
 const inputAtividade = document.createElement('input');
 const inputDataAtividade = document.createElement('input');
 const selectTagsPrioridade = document.createElement('select');
-// const optionTagsPrioridade1 = document.createElement('option');
-// const optionTagsPrioridade2 = document.createElement('option');
-// const optionTagsPrioridade3 = document.createElement('option');
-// const optionTagsPrioridade4 = document.createElement('option');
 const botao = document.createElement('button');
 
-// Atribuir Option ao Select
 const arrayOpcoes = ['Altíssima', 'Alta', 'Normal', 'Baixa'];
-arrayOpcoes.map((opcao, index) => {
+arrayOpcoes.forEach((opcao, index) => {
     const optionTagPropriedade = document.createElement('option');
     optionTagPropriedade.value = index;
-    // optionTagPropriedade.value = opcao;
     optionTagPropriedade.textContent = opcao;
     selectTagsPrioridade.appendChild(optionTagPropriedade);
 });
 selectTagsPrioridade.required = true;
-
-// optionTagsPrioridade1.textContent = 'Altissima';
-// selectTagsPrioridade.appendChild(optionTagsPrioridade1);
-// optionTagsPrioridade2.textContent = 'Alta';
-// selectTagsPrioridade.appendChild(optionTagsPrioridade2);
-// optionTagsPrioridade3.textContent = 'Normal';
-// selectTagsPrioridade.appendChild(optionTagsPrioridade3);
-// optionTagsPrioridade4.textContent = 'Baixa';
-// selectTagsPrioridade.appendChild(optionTagsPrioridade4);
 
 inputAtividade.id = 'input-atividade';
 inputAtividade.type = 'text';
@@ -45,24 +29,24 @@ inputDataAtividade.id = 'input-data-atividade';
 inputDataAtividade.type = 'date';
 inputDataAtividade.required = true;
 
-const formHandler = function (e) {
+botao.addEventListener('click', function (e) {
     e.preventDefault();
     const valorInputAtividade = inputAtividade.value;
     const valorInputDataAtividade = inputDataAtividade.value;
     const valorSelectTagsPrioridade = selectTagsPrioridade.value;
     const formularioEhValido = valorInputAtividade != '' && valorInputDataAtividade != '' && valorSelectTagsPrioridade != '';
     if (formularioEhValido) {
-      const novaOpcao = new Opcao(valorInputAtividade, valorInputDataAtividade, valorSelectTagsPrioridade);
-      listaAtividades.push(novaOpcao);
-      salvarLista();
-      renderizarItem(novaOpcao);
-      limparValorInput();
+        const novaOpcao = new Opcao(valorInputAtividade, valorInputDataAtividade, valorSelectTagsPrioridade);
+        listaAtividades.push(novaOpcao);
+        salvarLista();
+        renderizarItem(novaOpcao);
+        limparValorInput();
     } else {
-      alert('Preencher todos os campos');
+        alert('Preencher todos os campos');
     }
-  };
+    console.log(listaAtividades);
+});
 
-botao.addEventListener('click', formHandler);
 botao.textContent = 'Adicionar';
 botao.type = 'submit';
 
@@ -73,8 +57,6 @@ formulario.appendChild(botao);
 
 const divNovaAtividade = document.querySelector('.nova-atividade');
 divNovaAtividade.appendChild(formulario);
-
-////// LISTA ATIVIDADES
 const divListaAtividades = document.querySelector('.lista-atividades');
 
 const ulAtividades = document.createElement('ul');
@@ -91,8 +73,6 @@ function limparValorInput() {
 function renderizarItem(item) {
     const liAtividade = document.createElement('li');
     liAtividade.id = `item-${item.id}`;
-
-    //CRIAR CHECKBOX
     const inputCheckbox = document.createElement('input');
     inputCheckbox.id = item.id;
     inputCheckbox.type = 'checkbox';
@@ -118,7 +98,6 @@ function renderizarItem(item) {
     const paragrafoTag = document.createElement('p');
     const textoTag = arrayOpcoes[item.tagPrioridade];
     paragrafoTag.innerText = textoTag;
-    // paragrafoTag.innerText = item.tagPrioridade;
 
     liAtividade.appendChild(paragrafoTag);
 
@@ -131,9 +110,6 @@ function renderizarItem(item) {
     const botaoEditar = document.createElement('button');
     botaoEditar.innerText = 'Editar';
     liAtividade.appendChild(botaoEditar);
-    botaoEditar.addEventListener('click', function (e) {
-        editToDo(item);
-    });
 
     //CRIAR PARAGRAFO BOTAO EXCLUIR
     const botaoExcluir = document.createElement('button');
@@ -157,67 +133,9 @@ function baixarLista() {
     return JSON.parse(listaString) || [];
 }
 
-// function atualizarNome(nomeAntigo, nomeNovo) {
-//     listaAtividades = listaAtividades.map(lista => {
-//         if (lista.tagPrioridade === nomeAntigo) {
-//             lista.tagPrioridade = nomeNovo
-//         }
-//         return lista;
-//     })
-// }
-
 function excluirItemLista(id) {
     listaAtividades = listaAtividades.filter(atividade => atividade.id !== id);
     salvarLista();
     const itemRemovido = document.querySelector(`#item-${id}`);
     itemRemovido.remove();
 }
-
-function editToDo(item) {
-    let id = item.id;
-    const itemEditado = document.querySelector(`#item-${id}`);
-    inputAtividade.value = item.atividade;
-    inputDataAtividade.value = item.dataAtividade;
-    selectTagsPrioridade.value = item.tagPrioridade;
-  
-    botao.textContent = 'Editar';
-    botao.removeEventListener('click', formHandler);
-    botao.addEventListener('click', function (e) {
-      e.preventDefault();
-      const valorInputAtividade = inputAtividade.value;
-      const valorInputDataAtividade = inputDataAtividade.value;
-      const valorSelectTagsPrioridade = selectTagsPrioridade.value;
-      const formularioEhValido = valorInputAtividade != '' && valorInputDataAtividade != '' && valorSelectTagsPrioridade != '';
-      if (formularioEhValido) {
-        itemEditado.atividade = valorInputAtividade;
-        itemEditado.dataAtividade = valorInputDataAtividade;
-        itemEditado.tagPrioridade = valorSelectTagsPrioridade;
-        salvarLista();
-        renderizarItem(itemEditado);
-        limparValorInput();
-        botao.textContent = 'Adicionar';
-        botao.removeEventListener('click', formHandler);
-        botao.addEventListener('click', formHandler);
-      } else {
-        alert('Preencher todos os campos');
-      }
-    });
-  }
-  
-
-// Fazer com que o status do checkbox (marcado ou desmarcado) seja mantido após F5 na página
-// function marcarCheckbox(event, id) {
-//     listaAtividades = listaAtividades.filter(atividade => atividade.id !== id);
-//     salvarLista();
-//     const item = document.querySelector(`#item-${id}`);
-//     item.concluido = event.target.checked;
-//     localStorage.setItem(item.id, item.concluido);
-//   }
-
-// Aplicar estilo para grifar o texto da atividade caso ela seja marcada como concluida.
-// text-decoration: line-through;
-// UTILIZAR CLASSES!
-// CRIAR ARQUIVO CSS.
-// ADICIONAR O ARQUIVO CSS AO HTML.
-// ESTILIZAR O HTML COMO UM TODO.
-// SUBIR NO VERCEL OU SIMILAR!
--- exemplos/to-do-list/Opcao.js
+++ exemplos/to-do-list/Opcao.js
@@ -3,12 +3,18 @@ export class Opcao {
     atividade;
     dataAtividade;
     tagPrioridade;
-    constructor(atividade, dataAtividade, tagPrioridade) {
+    concluido;
+    constructor(atividade, dataAtividade, tagPrioridade, concluido) {
         this.id = Date.now().toString();
         this.atividade = atividade;
         this.dataAtividade = dataAtividade;
         this.tagPrioridade = tagPrioridade;
+        this.concluido = concluido;
     }
+
+    toggleCompletion() {
+        this.concluido = !this.concluido;
+      }
 }
 
 // module.exports = Opcao;
